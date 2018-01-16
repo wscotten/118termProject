649fd9a0f6ecf1040920e8e9cbf4158bbffb187c
William <wscotten@Williams-MacBook-Pro.local>
2018-01-15 22:07:13 -0800

    fixed snapshot test

--- src/__tests__/app2.test.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import App from '../App';
-
-it('snapshot test', () => {
-  const component = shallow(<App />);
-  expect(component).toMatchSnapshot();
-});
